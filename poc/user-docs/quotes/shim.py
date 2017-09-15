130209d9587db217260e44c3cb4c9d4b6950804e
Adrian Lucrèce Céleste <adrianlucrececeleste@airmail.cc>
2017-09-14 21:32:13 -0400

    [Add] shim program written in next section

---
+++ poc/user-docs/quotes/shim.py
@@ -0,0 +1,12 @@
+import io
+import sys
+import random
+from lxml import etree
+
+fp = io.TextIOWrapper(sys.stdin.buffer, encoding=sys.stdin.encoding)
+doc = etree.parse(fp)
+author = doc.xpath('/list/quote/author')
+date = doc.xpath('/list/quote/date')
+text = doc.xpath('/list/quote/text')
+ranquote = random.randrange(0,3)
+print("%s\n%s,\n%s" % (text[ranquote].text, author[ranquote].text, date[ranquote].text))
